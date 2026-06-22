84194c826320a2d4d201819b0af96798e3bb1224
Isaac-Nai <nai_nii@yahoo.co.uk>
2026-06-22 19:46:10 +0100

    Added a fetch function to the submit/sign-up button and seperated files into a frontend and a backend

---
+++ frontend/application.js
@@ -0,0 +1,46 @@
+const optionSection = document.querySelector(".option-section");
+const studentCard = document.querySelector(".student");
+const parentCard = document.querySelector(".parent")
+const studentForm = document.querySelector(".student-form");
+const parentForm = document.querySelector(".parent-form");
+
+studentCard.addEventListener("click", showStudentForm);
+parentCard.addEventListener("click", showParentForm);
+
+
+function showStudentForm() {
+    optionSection.classList.add("hidden");
+    console.log(optionSection.classList);
+    studentForm.classList.remove("hidden");
+}
+
+function showParentForm() {
+    optionSection.classList.add("hidden");
+    parentForm.classList.remove("hidden");
+}
+
+const submitBtn = document.querySelector(".form-grid")
+submitBtn.addEventListener("submit", request)
+
+async function request(event) {
+    event.preventDefault();
+
+    const formData = {
+        fname: document.querySelector("#fname").value,
+        lname: document.querySelector("#lname").value,
+        email: document.querySelector("#email").value,
+        tel: document.querySelector("#tel").value,
+        password: document.querySelector("#password").value,
+        dob: document.querySelector("#dob").value,
+    }
+
+    const res = await fetch("/api/sign-up", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formData)
+    }); 
+}
+
+
